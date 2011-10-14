c7a2fca69df274fc6b07e8c0bfe31dd3cf2a1551
Kelly Becker <kellylsbkr@gmail.com>
2011-10-14 00:00:14 -0400

    bundle update

--- _bundle.php
+++ _bundle.php
@@ -2,11 +2,15 @@
 
 namespace lhtml;
 
+require_once "parser.php";
+require_once "node.php";
+require_once "scope.php";
+
 class bundle {
 	
 	public static $hooks = array();
 	
-	public function add_hook($name, $obj) {
+	public function add_hook($name,&$obj) {
 		if(strpos($name, 0, 1) !== ':') throw new \Exception('You must prefix your LHTML hook with a colon! Error in hook $name');
 		self::$hooks[$name] =& $obj;
 	}
