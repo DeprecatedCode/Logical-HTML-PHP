92fdf6720340ccc220c24e13e3c7f7c4e2602748
Nate Ferrero <nateferrero@gmail.com>
2011-10-13 21:35:29 -0400

    Tokenizer

--- parser.php
+++ parser.php
@@ -5,12 +5,6 @@ use \Exception;
 
 class Parser2 {
 	
-	public $fileName;
-	public $lineNumber;
-	public $colNumber;
-	
-	public $cdataTags = array('script', 'style');
-	
 	// The entire syntax is defined here as what token the next char implies
 	private $tokenRules = array(
 		
@@ -24,7 +18,7 @@ class Parser2 {
 										'"' => '#error',
 										"'" => '#error',
 										'/' => 'tag-close',
-										'*' => '&tag-open-name' 		),
+										'*' => 'tag-open-name' 		),
 		# </_					
 		'tag-close'			=> array(	' ' => '#error',
 										'/' => '#error',
@@ -48,13 +42,13 @@ class Parser2 {
 										"'" => '#error',
 										'/' => '#error'				),
 		# <a ... _						
-		'tag-open-body'		=> array(	' ' => '#drop',
+		'tag-open-body'		=> array(	' ' => '#self',
 										'<' => '#error',
 										'>' => 'tag-end-inside',
 										'"' => '#error',
 										"'" => '#error',
 										'/' => 'tag-end-close',
-										'*' => '&tag-attr-name'		),
+										'*' => 'tag-attr-name'		),
 		# <a ... b_						
 		'tag-attr-name'		=> array(	' ' => '#error',
 										'<' => '#error',
@@ -68,7 +62,7 @@ class Parser2 {
 											'*' => '#error'			),
 		# <a ... b="_						
 		'tag-attr-quote'	=> array(	'"' => 'tag-attr-qend',
-										'*' => '&tag-attr-value'	),
+										'*' => 'tag-attr-value'	),
 		# <a ... b="c_						
 		'tag-attr-value'	=> array(	'escape' => '\\',
 										'"' => 'tag-attr-qend'		),
@@ -83,10 +77,13 @@ class Parser2 {
 										"'" => '#error',
 										'/' => '#error'				),
 		# <a ... />_ or </a>_							
-		'tag-end-outside'	=> array(	'*' => '&default'			),
-										
+		'tag-end-outside'	=> array(	'*' => 'default'			),
+		
+		# <a ... >_ or <a>_							
+		'tag-end-inside'	=> array(	'*' => 'tag-contents'		),
+						
 		# <a>_						
-		'tag-end-inside' => array(	'type' => 'conditional',
+		'tag-contents' 		=> array(	'type' => 'conditional',
 		
 			# <script...>_
 			array(	'token' 	=> 'tag-open-name',
@@ -135,11 +132,152 @@ class Parser2 {
 			throw new Exception($exception->getMessage() . " in file $file", 0, $exception);
 		}
 		
-		var_dump($tokens);die;
+		if(isset($_GET['--tokens'])) {
+			?>
+			<style>
+				body {
+					margin: 10px 50px 10px 20px;	
+				}
+				.tokens > div {
+					padding: 1px;
+					float: left;
+					white-space:pre;
+					position: relative;
+					font-size: 12px;
+					color: #000;
+					height: 15px;
+					background: #eee;
+					margin: 50px 1px;
+					font-family: monospace;
+					white-space:nowrap;
+				}
+				.tokens > div > span {
+					position: absolute;
+					font-size: 9px;
+					border-left: 1px solid #ccc;
+					left: -1px;
+				}
+				
+				/*Up*/
+				.tokens > div > span.x1 {	
+					top: -14px;
+					padding: 0 3px 19px;
+				}
+				.tokens > div > span.x3 {	
+					top: -26px;
+					padding: 0 3px 31px;
+				}
+				.tokens > div > span.x5 {	
+					top: -38px;
+					padding: 0 3px 43px;
+				}
+				
+				/*Down*/
+				.tokens > div > span.x2 {	
+					top: 0px;
+					padding: 18px 3px 0;
+				}
+				.tokens > div > span.x4 {	
+					top: 0px;
+					padding: 30px 3px 0;
+				}
+				.tokens > div > span.x6 {	
+					top: 0px;
+					padding: 42px 3px 0;
+				}
+				
+				/* Styles */
+				.tokens > div.blank {
+					background: #fee;
+					color: #fbb;
+				}
+				.tokens > div.tag-start, .tokens > div.tag-end-inside, .tokens > div.tag-end-outside {
+					background: #fff000;
+					color: #885;
+				}
+				.tokens > div.tag-open-name, .tokens > div.tag-close-name {
+					background: #fff999;
+					color: #885;
+				}
+				.tokens > div.tag-close, .tokens > div.tag-end-close {
+					background: #ffe999;
+					color: #000;
+				}
+				.tokens > div.tag-attr-quote, .tokens > div.tag-attr-qend {
+					background: #fbb;
+					color: #855;
+				}
+				.tokens > div.tag-attr-value {
+					background: #fdd;
+					color: #855;
+				}
+				.tokens > div.tag-attr-name {
+					background: #fed;
+					color: #865;
+				}
+				.tokens > div > b.newline {
+					background: #def;
+					color: #88f;
+					padding: 1px 2px 0;
+					margin: 0 1px;
+				}
+				.tokens > div > b.tab {
+					background: #dfe;
+					color: #4d4;
+					padding: 1px 20px 0;
+					margin: 0 1px;
+				}
+				.clear {
+					clear: left;
+					padding: 1px 0;
+				}
+			</style>
+			<div class="tokens">
+			<?php
+			$i = 0;
+			foreach($tokens as $token) {
+				$class = $token->name;
+				$v = htmlspecialchars($token->value);
+				switch($v) {
+					case '':
+						$v = '&empty;';
+						$class .= " blank";
+						break;
+				}
+				$v = str_replace(" ", '&nbsp;', $v);
+				$v = str_replace("\n", '<b class="newline">&crarr;</b></div><div class="clear">', $v);
+				$v = str_replace("\r", '<b class="newline">&crarr;</b></div><div class="clear">', $v);
+				$v = str_replace("\t", '<b class="tab">&raquo;</b>', $v);
+				$i++;
+				$pos = $i % 6 + 1;
+				echo "<div class='$class'><span class='x$pos'>$token->name</span>$v</div>";
+			}
+			
+			?></div><?php
+			die;
+		}
 		
-		// TODO GENERATE STACK FROM TOKENS - THIS KEEPS FEATURES SEPARATE
+		// GENERATE STACK FROM TOKENS - THIS KEEPS FEATURES SEPARATE
+		$openTags = array();
+		$openTagsDepth = 0;
+		$currentlyIn = '';
+		foreach($tokens as $token) {
+			
+			// Decide what to do based on token
+			switch($token->name) {
+				case 'tag-open-name':
+					$openTags[$openTagsDepth];
+				default:
+					continue;
+			}
+			if(!$stack && (!($stack instanceof Node))) $stack = new Node($name);
+			else $stack = $stack->_nchild($name);
+		}
 		
-		var_dump($stack);die;
+		
+		if(isset($_GET['--stack'])) {
+			var_dump($stack);die;
+		}
 		
 		// Return stack output
 		return $stack->output();
@@ -150,14 +288,18 @@ class Parser2 {
 		
 		// Reset line number
 		$lineNumber = 1;
-		$colNumber = 1;
+		$colNumber = 0;
+		
+		// Token start positions
+		$tokenLine = 1;
+		$tokenCol = 0;
 		
 		// Go through the code one char at a time, starting with default token
 		$length = strlen($source);
 		$tokens = array();
 		$queue = '';
 		$processImmediately = false;
-		for($pointer = 0; $pointer < $length; true) {
+		for($pointer = 0; $pointer <= $length; true) {
 			
 			// Check if processing a forwarded $char
 			if($processImmediately) {
@@ -169,16 +311,16 @@ class Parser2 {
 			// Else get a new $char
 			else {
 				
-				// Step ahead
-				$pointer++;
-				
-				// Get char
+				// Get char at pointer
 				$char = substr($source, $pointer, 1);
+				
+				// Step ahead after we have the char
+				$pointer++;
 				
 				// Increment line count
 				if($char == "\n" || $char == "\r") {
 					$lineNumber++;
-					$colNumber = 0;
+					$colNumber = -1;
 				}
 				
 				// Increment column count
@@ -210,11 +352,11 @@ class Parser2 {
 								continue;
 						
 							// Check that the token matches the condition, if set
-							if(isset($condtoken['token']) && $condtoken['token'] !== $last['token'])
+							if(isset($condtoken['token']) && $condtoken['token'] !== $last->name)
 								continue;
 								
 							// Check for matching value or catch-all condition
-							if(!isset($condtoken['value']) || $condtoken['value'] === $last['value']) {
+							if(!isset($condtoken['value']) || $condtoken['value'] === $last->value) {
 								
 								// Switch to this version of the token
 								$xtoken = $condtoken;
@@ -242,56 +384,61 @@ class Parser2 {
 			// Check if the current token has an action for this char, both literal and *
 			$literal = isset($xtoken[$checkchar]);
 			$star = isset($xtoken['*']);
-			if($literal || $star) {
-				
-				// Load the next token
-				$ntoken = $xtoken[$literal ? $checkchar : '*'];
-				
-				// Handle '#drop' token
-				if($ntoken === '#drop') {
-					continue;	
-				}
-				
-				// Handle '#error' token
-				if($ntoken === '#error') {
-					//var_dump($tokens);
-					//var_dump(array('token' => $token, 'queue' => $queue));
-					throw new Exception("Unexpected <code><b>'$char'</b></code>
-						after <i>$token</i> on line $lineNumber at column $colNumber, code: <code>$queue$char</code>");
-				}
-				
-				// Add the current token to the stack and handle queue
-				$tokens[] = array('token' => $token, 'value' => $queue);
-				
-				// Handle &tokens by immediately queueing the same char on the new token
-				if(substr($ntoken, 0, 1) === '&') {
-					$token = substr($ntoken, 1);
-					$processImmediately = true;
-				}
-				
-				// Normal tokens will start queue on next char
-				else {
-					$token = $ntoken;
-					$queue = '';
-				}
+			
+			// If no match, char is part of token and continue
+			if(!$literal && !$star) {
+				$queue .= $char;
+				continue;
 			}
 			
-			// If no match for character, add to queue
-			else {
+			// Load the next token
+			$ntoken = $xtoken[$literal ? $checkchar : '*'];
+			
+			// Handle '#drop' token
+			if($ntoken === '#drop') {
+				continue;	
+			}
+			
+			// Handle '#self' token
+			if($ntoken === '#self') {
 				$queue .= $char;
+				continue;	
+			}
+			
+			// Handle '#error' token
+			if($ntoken === '#error') {
+				//var_dump($tokens);
+				//var_dump(array('token' => $token, 'queue' => $queue));
+				return $tokens;
+				throw new Exception("Unexpected <code><b>'$char'</b></code>
+					after <i>$token</i> on line $lineNumber at column $colNumber, code: <code>$queue$char</code>");
+			}
+			
+			// Add the current token to the stack and handle queue
+			$tokens[] = (object) array('name' => $token, 'value' => $queue,
+				'line' => $tokenLine, 'col' => $tokenCol);
+			
+			// Update line and column for next token
+			$tokenLine = $lineNumber;
+			$tokenCol = $colNumber;
+			
+			// Handle &tokens by immediately queueing the same char on the new token
+			if(substr($ntoken, 0, 1) === '&') {
+				$token = substr($ntoken, 1);
+				$processImmediately = true;
+				$queue = '';
+			}
+			
+			// Normal tokens will start queue on next char
+			else {
+				$token = $ntoken;
+				$queue = $char;
 			}
 		}
 		
 		// Return tokens
 		return $tokens;
 	}
-	/*
-			
-			continue;
-			
-			if(!$stack && (!($stack instanceof Node))) $stack = new Node($name);
-			else $stack = $stack->_nchild($name);*/
-
 }
 
 // DELETE EVERYTHING BELOW THIS BLOCK
