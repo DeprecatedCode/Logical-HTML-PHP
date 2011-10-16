78c880bafd3781eec2b370acee35bf5e7465a58d
Kelly Becker <kellylsbkr@gmail.com>
2011-10-16 16:16:48 -0400

    bundlizing

--- _bundle.php
+++ _bundle.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace lhtml;
+namespace Evolution\LHTML;
+use Evolution\Kernel\Configure;
+use Evolution\Kernel\Service;
+use Evolution\Text\JSON;
+use Exception;
 
 require_once "parser.php";
 require_once "node.php";
 require_once "scope.php";
 
-class bundle {
+class Bundle {
 	
 	public static $hooks = array();
 	
