e58fc8d97bfefaf9e6391f31c05c070827fdb478
Nate Ferrero <nateferrero@gmail.com>
2011-10-22 04:42:49 -0400

    LTHML bundle load

--- _bundle.php
+++ _bundle.php
@@ -11,6 +11,20 @@ class Bundle {
 	private $file;
 	private $stack;
 	
+	public function __construct() {
+		/**
+		 * Add the site service
+		 */
+		Service::bind('Evolution\LHTML\Scope::addHook', 'lhtml:addhook');
+		Service::bind('Evolution\LHTML\Router::route', 'router:route:lhtml', 'portal:route:lhtml');
+		
+		/**
+		 * Add lhtml to default router and portal routing
+		 */
+		Configure::add('portal.defaults.run_with', 'lhtml');
+		Configure::add('router.defaults.run_with', 'lhtml');	
+	}
+	
 	public function file($file) {
 		$this->file = $file;
 		if($this->stack)
