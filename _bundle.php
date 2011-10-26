2f6ab1b354ad0ae120b1275e84e90750a243d5a4
Kelly Becker <kellylsbkr@gmail.com>
2011-10-26 06:58:13 -0400

    bringing up to speed

--- _bundle.php
+++ _bundle.php
@@ -15,14 +15,18 @@ class Bundle {
 		/**
 		 * Add the site service
 		 */
-		Service::bind('Evolution\LHTML\Scope::addHook', 'lhtml:addhook');
 		Service::bind('Evolution\LHTML\Router::route', 'router:route:lhtml', 'portal:route:lhtml');
 		
 		/**
 		 * Add lhtml to default router and portal routing
 		 */
 		Configure::add('portal.defaults.run_with', 'lhtml');
-		Configure::add('router.defaults.run_with', 'lhtml');	
+		Configure::add('router.defaults.run_with', 'lhtml');
+		Service::bind(array($this, '__bind_hooks'), 'site:ready');
+	}
+	
+	public function __bind_hooks() {
+		Service::run('lhtml:bind:hooks');
 	}
 	
 	public function file($file) {
